36aa12b4cc04348fffa559bafcfa674000d9c7e3
linuxjava <516282426@qq.com>
2014-05-23 08:55:04 +0800

    first commit

---
+++ src/csdn/shimiso/eim/activity/EimApplication.java
@@ -0,0 +1,32 @@
+package csdn.shimiso.eim.activity;
+
+import android.app.Activity;
+import android.app.Application;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import csdn.shimiso.eim.manager.XmppConnectionManager;
+
+/**
+ * 
+ * 完整的退出应用.
+ * 
+ * @author shimiso
+ */
+public class EimApplication extends Application {
+	private List<Activity> activityList = new LinkedList<Activity>();
+
+	// 添加Activity到容器中
+	public void addActivity(Activity activity) {
+		activityList.add(activity);
+	}
+
+	// 遍历所有Activity并finish
+	public void exit() {
+		XmppConnectionManager.getInstance().disconnect();
+		for (Activity activity : activityList) {
+			activity.finish();
+		}
+	}
+}
